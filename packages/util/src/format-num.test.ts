1740b34ba62c18e64e3a13d1dc9a0764dadbe175
wynnezheng <wynnezheng@tencent.com>
2021-08-30 19:55:05 +0800

    feat: utils add jest test

--- packages/util/src/format-num.test.ts
+++ packages/util/src/format-num.test.ts
@@ -7,8 +7,16 @@ function getRandomIntInclusive(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-test('formatNum', () => {
-  expect(formatNum(getRandomIntInclusive(0, 10000))).toMatch(/^\d+$/);
-  expect(formatNum(getRandomIntInclusive(10000, 1000000))).toMatch(/^\d+\.?\dw$/);
-  expect(formatNum(getRandomIntInclusive(1000000, 1000000 * 1000))).toMatch(/^99w\+$/);
-});
+describe('util/formatNum', () => {
+  test('< 10000', () => {
+    expect(formatNum(getRandomIntInclusive(0, 10000))).toMatch(/^\d+$/);
+  });
+
+  test('10000 < x < 1000000', () => {
+    expect(formatNum(getRandomIntInclusive(10000, 1000000))).toMatch(/^\d+\.?\dw$/);
+  });
+
+  test('> 1000000', () => {
+    expect(formatNum(getRandomIntInclusive(1000000, 1000000 * 1000))).toMatch(/^99w\+$/);
+  });
+})
